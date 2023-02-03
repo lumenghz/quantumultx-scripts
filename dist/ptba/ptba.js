1c4d25012bfc43aa00558cbf7239c2f9b9659ee3
lu.meng <lu.meng@danatech.cn>
2023-02-03 17:55:27 +0800

    chore: 1ptba add debug output

--- dist/ptba/ptba.js
+++ dist/ptba/ptba.js
@@ -28,8 +28,9 @@
   const store = useStore();
   const is = useIs();
   if (is.isRequest && cookieRegex.test($request.url)) {
+    $notify("DEBUG", "1ptba", JSON.stringify($request.headers));
     const cookie = $request == null ? void 0 : $request.headers.cookie;
-    if (cookie !== "undefined") {
+    if (cookie !== void 0) {
       store.write("1ptba_cookie", cookie);
       $notify("cookie更新成功", "1ptba", `更新为 ${cookie}`);
     }
