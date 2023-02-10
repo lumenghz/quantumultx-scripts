befcd161445628c415457f421b7e9d5d598e8aae
Dennis Lu <jiahehz@gmail.com>
2023-02-10 22:20:32 +0800

    chore: upate request

--- dist/ptba/ptba.js
+++ dist/ptba/ptba.js
@@ -24,13 +24,13 @@
       }
     };
   }
-  const cookieRegex = /https:\/\/1ptba.com\/takelogin.php/;
+  const cookieRegex = /https:\/\/1ptba.com\/index.php/;
   const store = useStore();
   const is = useIs();
   if (is.isRequest && cookieRegex.test($request.url)) {
     $notify("DEBUG", "1ptba", JSON.stringify($request.headers));
-    const cookie = $request == null ? void 0 : $request.headers.cookie;
-    if (cookie !== void 0) {
+    const cookie = $request == null ? void 0 : $request.headers.Cookie;
+    if ($request.method === "GET" && cookie !== void 0) {
       store.write("1ptba_cookie", cookie);
       $notify("cookie更新成功", "1ptba", `更新为 ${cookie}`);
     }
