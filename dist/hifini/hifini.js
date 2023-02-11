44d0f0329271b7fb62bafd139a930d421b9fe738
Dennis Lu <jiahehz@gmail.com>
2023-02-12 02:12:13 +0800

    chore: hifini use more precision regex

--- dist/hifini/hifini.js
+++ dist/hifini/hifini.js
@@ -27,16 +27,13 @@
   const store = useStore();
   const is = useIs();
   const prefKey = "hifini_cookie";
-  const cookieRegex = /https:\/\/hifini.com/;
+  const cookieRegex = /^https:\/\/hifini.com\/$/;
   if (is.isRequest && cookieRegex.test($request.url)) {
     const cookie = $request == null ? void 0 : $request.headers.cookie;
     if ($request.method === "GET") {
       if (cookie !== void 0) {
-        console.log(cookie);
         store.write(prefKey, cookie);
         $notify("cookie更新成功", "hifini", `更新为 ${cookie}`);
-      } else {
-        $notify("hifini no cookie");
       }
     }
     $done();
